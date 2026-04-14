fdb5c4387e4361cc8ccf7f66eb327029a40e76d2
Vedaang Sharma <vedaangsharma2006@gmail.com>
2026-04-15 00:10:28 +0530

    Update models.dart

--- lib/models/models.dart
+++ lib/models/models.dart
@@ -1,3 +1,31 @@
+class UserProfile {
+  final String name;
+  final String email;
+  final String? phone;
+  final String? bio;
+  final String campusLocation;
+  final double rating;
+  final int reviews;
+  final int activeListings;
+  final int soldItems;
+  final int draftListings;
+  final bool verified;
+
+  const UserProfile({
+    required this.name,
+    required this.email,
+    this.phone,
+    this.bio,
+    required this.campusLocation,
+    required this.rating,
+    required this.reviews,
+    required this.activeListings,
+    required this.soldItems,
+    required this.draftListings,
+    required this.verified,
+  });
+}
+
 class Seller {
   final String id;
   final String name;
@@ -6,6 +34,7 @@ class Seller {
   final int listings;
   final int sold;
   final bool verified;
+  final String? bio;
 
   const Seller({
     required this.id,
@@ -15,6 +44,7 @@ class Seller {
     required this.listings,
     required this.sold,
     required this.verified,
+    this.bio,
   });
 }
 
@@ -28,6 +58,14 @@ class Product {
   final String location;
   final String description;
   bool isSaved;
+  final bool isSold;
+  final bool isDraft;
+  final int views;
+  final int favorites;
+  final int messages;
+  final String listedAt;
+  final bool isNegotiable;
+  final double? minOffer;
 
   Product({
     required this.id,
@@ -39,6 +77,14 @@ class Product {
     required this.location,
     required this.description,
     this.isSaved = false,
+    this.isSold = false,
+    this.isDraft = false,
+    this.views = 0,
+    this.favorites = 0,
+    this.messages = 0,
+    this.listedAt = '1 day ago',
+    this.isNegotiable = false,
+    this.minOffer,
   });
 }
 
@@ -61,12 +107,18 @@ class Conversation {
   final Seller seller;
   final String lastMessage;
   final List<Message> messages;
+  final String time;
+  final bool hasUnread;
+  final Product? product;
 
   const Conversation({
     required this.id,
     required this.seller,
     required this.lastMessage,
     required this.messages,
+    this.time = '',
+    this.hasUnread = false,
+    this.product,
   });
 }
 
@@ -76,4 +128,16 @@ class Category {
   final String emoji;
 
   const Category({required this.id, required this.name, required this.emoji});
+}
+
+class BlockedUser {
+  final String id;
+  final String name;
+  final String blockedAt;
+
+  const BlockedUser({
+    required this.id,
+    required this.name,
+    required this.blockedAt,
+  });
 }
